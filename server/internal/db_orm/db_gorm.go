835abf435cedd3dab1dc721d341a6c837fbe3631
pavva91 <valerio.mattioli@proton.me>
2024-09-13 20:56:43 +0200

    DB ORM

---
+++ server/internal/db_orm/db_gorm.go
@@ -0,0 +1,49 @@
+package db_orm
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/pavva91/bike-backend/server/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var (
+	ORM      ORMer = orm{}
+	database *gorm.DB
+)
+
+type ORMer interface {
+	MustConnectToDB(cfg config.ServerConfig)
+	GetDB() *gorm.DB
+}
+
+type orm struct{}
+
+func (o orm) MustConnectToDB(cfg config.ServerConfig) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=%s",
+		cfg.Database.Host,
+		cfg.Database.Username,
+		cfg.Database.Password,
+		cfg.Database.Name,
+		cfg.Database.Port,
+		cfg.Database.Timezone,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		log.Println(err.Error())
+		panic(fmt.Errorf("error connecting db: %w", err))
+	}
+
+	db.Logger.LogMode(logger.Info)
+	database = db
+}
+
+func (o orm) GetDB() *gorm.DB {
+	return database
+}
+
