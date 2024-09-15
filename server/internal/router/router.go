1a90eeb247ff6932811e6f527e7da0fc674b2cd6
pavva91 <valerio.mattioli@proton.me>
2024-09-15 18:45:13 +0200

    initial router

---
+++ server/internal/router/router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/pavva91/bike-backend/server/internal/handlers"
+)
+
+var Router *mux.Router
+
+func NewRouter() {
+	Router = mux.NewRouter()
+
+	initializeRoutes()
+}
+
+func initializeRoutes() {
+	files := Router.PathPrefix("/users").Subrouter()
+	files.HandleFunc("", handlers.AccountsHandler.List).Methods("GET")
+	files.HandleFunc("/", handlers.AccountsHandler.List).Methods("GET")
+	files.HandleFunc("/{id:[0-9]+}", handlers.AccountsHandler.Get).Methods("GET")
+}
